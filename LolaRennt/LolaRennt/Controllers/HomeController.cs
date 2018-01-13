5a97ef69f80e5c17dde0197d4221ff9b798f5411
chif84 <chif.84@mail.ru>
2018-01-13 11:54:04 +0400

    первая заливка сервера

---
+++ LolaRennt/LolaRennt/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace LolaRennt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FinishedRaces()
+        {
+            return View();
+        }
+
+        public ActionResult FinishedRace(string roomKey)
+        {
+            return View("FinishedRace", (object)roomKey);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LolaRennt/LolaRennt/Scripts/firebaseconfig.js
@@ -0,0 +1,7 @@
+﻿var fbConfig = {
+    apiKey: "vrwQwApj78sb2MpxTAVd44aMdmhUTnoIY0LR4FwG",
+    authDomain: "tstapp-eedbc.firebaseapp.com",
+    databaseURL: "https://tstapp-eedbc.firebaseio.com"
+};
+
+firebase.initializeApp(fbConfig);
